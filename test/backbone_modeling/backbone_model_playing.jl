50579beac348a34d7a1db28801e8aaad07264cac
Cris Moen <cris.moen@runtosolve.com>
2023-06-21 17:54:23 +0300

    work on backbone function

--- test/backbone_modeling/backbone_model_playing.jl
+++ test/backbone_modeling/backbone_model_playing.jl
@@ -2,7 +2,7 @@
 using JSON3, Dates, StructTypes, CairoMakie
 
 #get file from repo
-json_string = read(download("https://raw.githubusercontent.com/cfsrc/FastenerConnectionData/main/data/Zhang_2020_11.json"), String)
+json_string = read(download("https://raw.githubusercontent.com/cfsrc/FastenerConnectionData/main/data/Zhang_2020_23.json"), String)
 
 
 #define data structure
@@ -56,6 +56,41 @@ f = Figure()
 Axis(f[1, 1])
 scatterlines!(specimen.test.displacement, specimen.test.force)
 f
+
+P3 = maximum(specimen.test.force)
+Δ3 = specimen.test.displacement[argmax(specimen.test.force)]
+
+P2 = 0.8 * P3
+
+P_pre_peak_indices = findall(disp->disp<=Δ3, specimen.test.displacement)
+P_pre_peak = specimen.test.force[P_pre_peak_indices]
+
+difference = abs.(P_pre_peak .- 0.8 * P3)
+
+sorted_difference = sort(difference)
+
+closest_difference = sorted_difference[1:2]
+
+closest_index = [findall(diff->diff==closest_difference[i], difference) for i in eachindex(closest_difference)]
+
+δ1 = specimen.test.displacement[closest_index[1]][1]
+δ2 = specimen.test.displacement[closest_index[2]][1]
+
+p1 = specimen.test.force[closest_index[1]][1]
+p2 = specimen.test.force[closest_index[2]][1]
+
+slope = (p2 - p1) \ (δ2 - δ1)
+
+Δ2 = δ1 + P2 \ slope
+
+#need P1, Δ1 
+#need P4, Δ4
+#need P5, Δ5
+
+
+
+
+
 
 
 #find peak
